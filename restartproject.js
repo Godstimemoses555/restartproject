e459c008d42f971477186c26aff1b2fa28e8eadd
Godstimemoses555 <inyanggodstime63@gmail.com>
2025-08-01 12:38:17 -0700

    completed version 1 of restartproject

---
+++ restartproject.js
@@ -0,0 +1,16 @@
+const fred = document.querySelector(".hambugerimg")
+const budy = document.querySelector(".closediv")
+
+const func1=()=>{
+  fred.style.display = "none"
+budy.style.display = "flex"
+
+}
+
+const func2=()=>{
+    budy.style.display = "none"
+fred.style.display = "flex"
+
+}
+
+
